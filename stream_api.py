19de61ae911b8742f68652562c95161f47397005
Myf Ma <myf@worldtalks.org>
2013-11-04 00:10:05 -0500

    pymongo changes data itself when insert, wtf

--- stream_api.py
+++ stream_api.py
@@ -56,11 +56,13 @@ class StdOutListener(StreamListener):
                 #check if it's in nyc bound
                 ne = [40.92285206859968, -73.66264343261719]
                 sw = [40.558156335842106, -74.27444458007812]
-                if _check_bounds(coor, ne, sw):
-                    collection.insert(data_insert)
-                    print "inserted: %s" % text
-                    send_to_socket(data_insert)
-                    # send data to a tcp server interfacing with node
+                #if _check_bounds(coor, ne, sw):
+                print "inserted: %s" % text
+                send_data = json.dumps(data_insert)
+                send_to_socket(send_data)
+                print "message sent!"
+                collection.insert(data_insert)
+                # send data to a tcp server interfacing with node
 
             
         except BaseException, e:
@@ -76,8 +78,7 @@ def _check_bounds(coordinates, ne, sw):
 def send_to_socket(data):
     client = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
     client.connect(sock_loc)
-    send_data = json.dumps(data)
-    client.send(send_data)
+    client.send(data)
     client.close()
 
 
@@ -88,10 +89,11 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
  
 stream = Stream(auth, listener)
-track_list = ['NYC']        
+track_list = ['party']        
 try:
     stream.filter(track=track_list)
 except KeyboardInterrupt:
     print 'Interupted'
     sys.exit(0)
+
 
